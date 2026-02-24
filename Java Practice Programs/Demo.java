61c5d03c02535bee9a5b0bbfd95207a22c6e4d1f
Asadullah Shaikh <asadullahsaeed12345@gmail.com>
2026-02-24 21:09:52 +0500

    Demo.java

--- Java Practice Programs/Demo.java
+++ Java Practice Programs/Demo.java
@@ -1,16 +1,21 @@
 public class Demo{
 public static void main(String[] args){
-    float PI=3.14f, radius=7.2f;
-    float area=PI*radius*radius;
-    System.out.println("The area of the circle is: "+area);
+    // Sum of two numbers
     // int n1=60, n2=20, s;
     // s=n1+n2;
     // System.out.println("60 + 20 = "+s);
-    // for (int i = 0; i < 10; i++) {
-    //     for (int j = 0; j <= 9-i; j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }   
+    
+    // Finding Area of Circle
+    // float PI=3.14f, radius=7.2f;
+    // float area=PI*radius*radius;
+    // System.out.println("The area of the circle is: "+area);
+
+    // Printing the right triangle using nested for loop    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }   
 }
 }
