141bd969224d3396256cded8f6b23960a9892932
Asadullah Shaikh <asadullahsaeed12345@gmail.com>
2026-03-02 11:08:28 +0500

    Update Number_Guessing_Game.java

--- Java Projects/Number_Guessing_Game.java
+++ Java Projects/Number_Guessing_Game.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+public class Number_Guessing_Game {
+    public static void main(String[] args) {
+        int guess = 0, c=0;
+        int correct_guess = 245;
+        Scanner sc = new Scanner(System.in);
 
+        while(guess!=correct_guess){
+            System.out.print("Guess the number and the number pattern is _ _ _ : ");
+            guess = sc.nextInt();
+            if(guess<correct_guess){
+                System.out.println("Too Low");
+            }
+            else if(guess>correct_guess){
+                System.out.println("Too High");
+            }
+            c++;
+        }
+        System.out.println("You are right its "+correct_guess);
+        System.out.println("The number of guesses you have taken are: "+c);
+        sc.close();
+    }
+}
